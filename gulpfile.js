92a0eeb31d03a9bb41728c038974e5588b7056a7
Leigh Simpson <code@simpleigh.com>
2015-12-30 12:24:22 +0000

    Added gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,5 @@ var gulp = require('gulp'),
     shell = require('gulp-shell');
 
 gulp.task('default', shell.task(path.join('bin', 'tsc')));
+
+gulp.task('watch', shell.task(path.join('bin', 'tsc') + ' --watch'));
