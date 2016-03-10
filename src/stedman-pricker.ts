1de8d57e9fa6de384e8122d4ceb3d4abbcf35168
Leigh Simpson <code@simpleigh.com>
2016-03-10 19:40:38 +0000

    Corrected spelling of "GPL" in licence definition

--- src/stedman-pricker.ts
+++ src/stedman-pricker.ts
@@ -1,7 +1,7 @@
 /**
  * Free Stedman Pricker
  * @author Leigh Simpson <code@simpleigh.com>
- * @license GLP-3.0
+ * @license GPL-3.0
  * @copyright © 2015 Leigh Simpson. All rights reserved.
  */
 
