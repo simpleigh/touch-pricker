3c6e0c150fbe85ad09cc5d688bd403f678755d8c
Leigh Simpson <code@simpleigh.com>
2016-03-26 10:25:25 +0000

    First implementation of Touch class (container for Courses)

--- src/stedman-pricker.ts
+++ src/stedman-pricker.ts
@@ -561,6 +561,52 @@ namespace Pricker {
         }
     }
 
+    /**
+     * A touch, being a set of courses
+     */
+    export class Touch extends AbstractContainer<Course> {
+
+        /* AbstractContainer methods ******************************************/
+
+        /**
+         * Returns the default length of new containers of this type
+         * 
+         * Derived classes should override this method if required.
+         */
+        protected getDefaultLength(initialRow: Row): number {
+            return 0;
+        }
+
+        /**
+         * Creates a new block for the container
+         * 
+         * Used by extend() when creating the container or increasing its
+         * length.
+         * @param {Row}     initialRow - initial row for the block
+         * @param {number}  index      - index of block in container
+         */
+        protected createBlock(initialRow: Row, index: number): Course {
+            return new Course(initialRow, this, index);
+        }
+
+        /**
+         * Returns the limits on length for the particular concrete class
+         * 
+         * minimum, maximum
+         */
+        protected getLengthLimits(): [number, number] {
+            return [0, 100];
+        }
+
+        /* Touch methods ******************************************************/
+
+        /**
+         * Read access to courses
+         */
+        public getCourse(course: number): Course {
+            return this.getBlock(course);
+        }
+    }
 
     /**
      * Classes for output
---
+++ tests/specs/Touch.spec.ts
@@ -0,0 +1,17 @@
+describe('Touch class', function () {
+
+    testAbstractBlockImplementation(Pricker.Touch);
+
+    testAbstractContainerImplementation(
+        Pricker.Touch,
+        'getCourse',
+        [
+            [Pricker.Stage.Triples, 0],
+            [Pricker.Stage.Caters, 0],
+            [Pricker.Stage.Cinques, 0],
+            [Pricker.Stage.Sextuples, 0],
+            [Pricker.Stage.Septuples, 0],
+        ],
+        [0, 100]
+    );
+});
