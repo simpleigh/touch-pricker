193569a2a04786dc17c3a2c381e194c0446ca4ef
Leigh Simpson <leigh.simpson@fusepump.com>
2017-03-31 21:14:23 +0100

    Replaced typings with direct dependencies on @types/* packages

--- tests/jasmine.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Free Stedman Pricker
- * @author Leigh Simpson <code@simpleigh.com>
- * @license GPL-3.0
- * @copyright © 2015-17 Leigh Simpson. All rights reserved.
- */
-
-/// <reference path="../typings/globals/jasmine/index.d.ts" />
