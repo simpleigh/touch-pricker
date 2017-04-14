5277bf59d4c45340c3cd3a2cfad8dba4db3c4405
Leigh Simpson <code@simpleigh.com>
2017-04-14 22:30:53 +0100

    Implemented import of touches from strings

--- src/Touch.ts
+++ src/Touch.ts
@@ -7,6 +7,9 @@
 
 /// <reference path="Row.ts" />
 /// <reference path="Changes.ts" />
+/// <reference path="Course.ts" />
+/// <reference path="Stage.ts" />
+/// <reference path="stringFromRow.ts" />
 /// <reference path="Visitor/Abstract.ts" />
 
 namespace Pricker {
@@ -110,6 +113,57 @@ namespace Pricker {
             for (let i: number = index; i <= this.getLength(); i++) {
                 this.getCourse(i).setOwnership(this, i);
             }
+        }
+
+        /**
+         * Creates a new touch from a string representation
+         */
+        public static fromString(input: string): Touch {
+            const inputLines: string[] = input.split('\n'),
+                processedLines: string[] = [ ];
+
+            let i: number,
+                inputLine: string,
+                firstLine: string | undefined,
+                course: Course,
+                touch: Touch;
+
+            // Process each input line, making text substitutions
+            for (i = 0; i < inputLines.length; i++) {
+                inputLine = inputLines[i];
+
+                // Drop any content after comment characters "//"
+                inputLine = inputLine.replace(/\/\/.*$/, '');
+
+                // Ignore a microsiril comment "/" at the start of a line
+                inputLine = inputLine.replace(/^\//, '');
+
+                // Skip this line if it's blank
+                if (/^\s*$/.test(inputLine)) {
+                    continue;
+                }
+
+                processedLines.push(inputLine);
+            }
+
+            // Create the touch with a stage based on the first line
+            firstLine = processedLines.shift();
+            if (!firstLine) {
+                throw new Error('No input lines');
+            }
+            firstLine.replace(/\s/g, '');
+            if (!Stage[firstLine.length]) {
+                throw new Error('Cannot recognise stage');
+            }
+            touch = new Touch(rowFromString('231', firstLine.length));
+
+            // Create a course for each remaining line
+            for (i = 0; i < processedLines.length; i++) {
+                course = Course.fromString(touch.getEnd(), processedLines[i]);
+                touch.insertCourse(touch.getLength() + 1, course);
+            }
+
+            return touch;
         }
     }
 }
--- tests/specs/Touch.spec.ts
+++ tests/specs/Touch.spec.ts
@@ -9,6 +9,13 @@
 
 describe('Touch class', function () {
 
+    function testImport(input, output) {
+        return function () {
+            const touch: Pricker.Touch = Pricker.Touch.fromString(input);
+            expect(touch.print('text')).toBe(output);
+        };
+    }
+
     it('generates the correct rows when visited', function () {
         const stage: Pricker.Stage = Pricker.Stage.Cinques,
             initialRow: Pricker.Row = Pricker.rowFromString('231', stage),
@@ -29,6 +36,88 @@ describe('Touch class', function () {
         touch.accept(visitor);
 
         expect(visitor.getStrings()).toEqual(strings);
+    });
+
+    describe('can create touches from strings:', function () {
+
+        it('a simple touch', testImport(
+            '2314567890E\n'
+                + '2314567890E  1 s10 s13 22\n',
+            '2314567890E\n'
+                + '2314567890E  1 s10 s13 22\n',
+        ));
+
+        it('a touch with more than one course', testImport(
+            '2314567890E\n'
+                + '2314568790E  1 s10 s13 s15 22\n'
+                + '2314567890E  1 s10 s13 s15 22\n',
+            '2314567890E\n'
+                + '2314568790E  1 s10 s13 s15 22\n'
+                + '2314567890E  1 s10 s13 s15 22\n',
+        ));
+
+        it('a touch that comes round at hand', testImport(
+            '2314567890E\n'
+                + '21436578E90  1 5 7 8 10 11 s13 15 16  (20 sixes)\n'
+                + '2143658709E  2 s13 s15\n'
+                + '2143658709E  p\n',
+            '2314567890E\n'
+                + '21436578E90  1 5 7 8 10 11 s13 15 16  (20 sixes)\n'
+                + '2143658709E  2 s13 s15\n'
+                + '2143658709E  p\n',
+        ));
+
+        it('a touch with a blank line', testImport(
+            '2314567890E\n'
+                + ' \t\n'
+                + '2314567890E  1 s10 s13 22\n',
+            '2314567890E\n'
+                + '2314567890E  1 s10 s13 22\n',
+        ));
+
+        it('a touch with microsiril comments', testImport(
+            '2314567890E\n'
+                + '/2314567890E  1 s10 s13 22\n',
+            '2314567890E\n'
+                + '2314567890E  1 s10 s13 22\n',
+        ));
+
+        it('a touch with a "//" comment line', testImport(
+            '2314567890E\n'
+                + '// comment\n'
+                + '2314567890E  1 s10 s13 22\n',
+            '2314567890E\n'
+                + '2314567890E  1 s10 s13 22\n',
+        ));
+
+        it('a touch with an included "//" comment', testImport(
+            '2314567890E\n'
+                + '2314567890E  1 s10 s13 22  // turn 78\n',
+            '2314567890E\n'
+                + '2314567890E  1 s10 s13 22\n',
+        ));
+
+        it('a touch with no lines', function () {
+            expect(function () {
+                Pricker.Touch.fromString('');
+            }).toThrowError('No input lines');
+        });
+
+        it('a touch with a broken initial row', function () {
+            expect(function () {
+                Pricker.Touch.fromString('not');
+            }).toThrowError('Cannot recognise stage');
+        });
+
+        it('a touch with a broken course', function () {
+            expect(function () {
+                Pricker.Touch.fromString(
+                    '2314567890E\n'
+                        + 'garbage\n',
+                );
+            }).toThrowError('Cannot import course');
+        });
+
     });
 
     testAbstractContainerImplementation(
