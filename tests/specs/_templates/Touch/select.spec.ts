ed2c2f615101e59996f5fc9d1a46a7b92e2ae474
Leigh Simpson <code@simpleigh.com>
2017-04-15 16:41:10 +0100

    Simplified MBD example by rendering <select> box via template

---
+++ tests/specs/_templates/Touch/select.spec.ts
@@ -0,0 +1,30 @@
+/**
+ * Free Stedman Pricker
+ * @author Leigh Simpson <code@simpleigh.com>
+ * @license GPL-3.0
+ * @copyright © 2015-17 Leigh Simpson. All rights reserved.
+ */
+
+describe('select template for Touch', function () {
+
+    it('renders a touch correctly', function () {
+        const text: string = '2314567890E\n'
+                + '2314568790E  1 s10 s13 s15 22\n'
+                + '2314567890E  1 s10 s13 s15 22\n',
+            touch: Pricker.Touch = Pricker.Touch.fromString(text);
+
+        expect(touch.print('select')).toBe(
+            ''
+                + '<option value="0">'
+                + Pricker.stringFromRow(touch.getInitialRow())
+                + '</option>'
+                + '<option value="1">'
+                + touch.getCourse(1).print('text')
+                + '</option>'
+                + '<option value="2">'
+                + touch.getCourse(2).print('text')
+                + '</option>',
+        );
+    });
+
+});
