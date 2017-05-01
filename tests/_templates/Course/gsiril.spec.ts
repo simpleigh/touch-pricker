abeeeb713d85f4de6feb0175839f642b59144b07
Leigh Simpson <code@simpleigh.com>
2017-05-01 18:40:09 +0100

    Modified gsiril template for Course to stop rendering based on the number of rows in the touch

--- tests/_templates/Course/gsiril.spec.ts
+++ tests/_templates/Course/gsiril.spec.ts
@@ -18,4 +18,42 @@ describe('gsiril template for Course', function () {
         );
     });
 
+    const EXPECTED_OUTPUTS: string[] = [
+        '',  // length zero not tested
+        'plain,',
+        'plain,+3,',
+        'plain,+3.1,',
+        'plain,+3.1.3,',
+        'plain,+3.1.3.1,',
+        'plain,slow,',
+        'plain,slow,single,',
+        'plain,slow,single,+1,',
+        'plain,slow,single,+1.3,',
+        'plain,slow,single,+1.3.1,',
+        'plain,slow,single,+1.3.1.3,',
+        'plain,slow,single,quick,',
+        'plain,slow,single,quick,bob,',
+        'plain,slow,single,quick,bob,+3,',
+        'plain,slow,single,quick,bob,+3.1,',
+        'plain,slow,single,quick,bob,+3.1.3,',
+        'plain,slow,single,quick,bob,+3.1.3.1,',
+        'plain,slow,single,quick,bob,slow,',
+        'plain,slow,single,quick,bob,slow,plain,',
+        'plain,slow,single,quick,bob,slow,plain,+1,',
+        'plain,slow,single,quick,bob,slow,plain,+1.3,',
+        'plain,slow,single,quick,bob,slow,plain,+1.3.1,',
+        'plain,slow,single,quick,bob,slow,plain,+1.3.1.3,',
+        'plain,slow,single,quick,bob,slow,plain,quick,',
+    ];
+
+    it('stops rendering based on the remaining rows in the touch', function () {
+        const course = Pricker.Course.fromString(createTestRow(), 's2 3 (4)');
+        for (let i: number = 1; i < EXPECTED_OUTPUTS.length; i += 1) {
+            expect(course.print('gsiril', {'touchRows': i})).toBe(
+                EXPECTED_OUTPUTS[i]
+                    + '"@  480735692E1  s2 3  (4 sixes)"\n',
+            );
+        }
+    });
+
 });
