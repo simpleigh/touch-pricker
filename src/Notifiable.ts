09a29c2ecbd977dadfb748504b8cfc6864380936
Leigh Simpson <leigh.simpson@fusepump.com>
2017-05-12 21:53:30 +0100

    Replaced © symbol in copyright header as sourcemaps can't cope with the encoding

--- src/Notifiable.ts
+++ src/Notifiable.ts
@@ -2,7 +2,7 @@
  * Free Stedman Pricker
  * @author Leigh Simpson <code@simpleigh.com>
  * @license GPL-3.0
- * @copyright © 2015-17 Leigh Simpson. All rights reserved.
+ * @copyright Copyright 2015-17 Leigh Simpson. All rights reserved.
  */
 
 namespace Pricker {
