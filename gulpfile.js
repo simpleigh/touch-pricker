72a3e21dcc7fbc90d1f38ec2c9b1ba8ea32e6308
Leigh Simpson <code@simpleigh.com>
2018-04-16 08:23:56 +0100

    Updated year in copyright notices

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * Free Touch Pricker
  * @author Leigh Simpson <code@simpleigh.com>
  * @license GPL-3.0
- * @copyright Copyright 2015-17 Leigh Simpson. All rights reserved.
+ * @copyright Copyright 2015-18 Leigh Simpson. All rights reserved.
  */
 
 /*global: require*/
