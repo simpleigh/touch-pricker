f08d0a0186fd7d88710cf6482d168f74f4646723
Leigh Simpson <code@simpleigh.com>
2018-04-28 11:56:39 +0100

    Modified Course to support starting with either quick or slow sixes

---
+++ src/SixType.ts
@@ -0,0 +1,15 @@
+/**
+ * Free Touch Pricker
+ * @author Leigh Simpson <code@simpleigh.com>
+ * @license GPL-3.0
+ * @copyright Copyright 2015-18 Leigh Simpson. All rights reserved.
+ */
+
+namespace Pricker {
+
+    /**
+     * Types of six
+     * @enum {number}
+     */
+    export enum SixType { Slow = 0, Quick }
+}
