4844194cfcdcd82e9bfd43ead7e8ad933b82fae7
Leigh Simpson <code@simpleigh.com>
2018-07-11 20:10:39 +0100

    Added watch target for Jasmine SpecRunner

--- config/webpack.config.test.js
+++ config/webpack.config.test.js
@@ -5,6 +5,11 @@ const banner = require('./webpack.banner');
 const base = require('./webpack.base');
 
 module.exports = merge(base, {
+    devServer: {
+        openPage: 'tests/SpecRunner.html',
+        port: 8081,
+        publicPath: '/dist/',
+    },
     entry: paths.testsEntryFile,
     output: { filename: 'touch-pricker.spec.js' },
 }, banner);
