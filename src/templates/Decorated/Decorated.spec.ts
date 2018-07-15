e4fae22902fa4846a149e357d3cdfd865a443987
Leigh Simpson <code@simpleigh.com>
2018-07-15 15:44:51 +0100

    Removed test example of new templating implementation

--- src/templates/Decorated/Decorated.spec.ts
+++
@@ -1,8 +0,0 @@
-import Decorated from './Decorated';
-
-describe('Decorated class', () => {
-    it('returns data', () => {
-        const decorated = new Decorated();
-        expect(decorated.getData()).toBe('test data');
-    });
-});
--- src/templates/Decorated/text.spec.ts
+++
@@ -1,8 +0,0 @@
-import Decorated from './Decorated';
-
-describe('text template for Decorated', () => {
-    it('renders the data', () => {
-        const decorated = new Decorated();
-        expect(decorated.print('text')).toBe('test data');
-    });
-});
