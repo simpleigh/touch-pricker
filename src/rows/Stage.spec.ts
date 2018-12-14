6d91cc592a7817aa3ac88bb7b048642d005a1396
Leigh Simpson <code@simpleigh.com>
2018-12-14 20:00:51 +0000

    Filled in missing stages up to 16

--- src/rows/Stage.spec.ts
+++ src/rows/Stage.spec.ts
@@ -10,19 +10,35 @@ import Stage from './Stage';
 describe('Stage enum', () => {
 
     it('maps stage names to numbers of bells', () => {
+        expect(Stage.Minimus).toBe(4);
+        expect(Stage.Doubles).toBe(5);
+        expect(Stage.Minor).toBe(6);
         expect(Stage.Triples).toBe(7);
+        expect(Stage.Major).toBe(8);
         expect(Stage.Caters).toBe(9);
+        expect(Stage.Royal).toBe(10);
         expect(Stage.Cinques).toBe(11);
+        expect(Stage.Maximus).toBe(12);
         expect(Stage.Sextuples).toBe(13);
+        expect(Stage.Fourteen).toBe(14);
         expect(Stage.Septuples).toBe(15);
+        expect(Stage.Sixteen).toBe(16);
     });
 
     it('maps numbers of bells to stage names', () => {
+        expect(Stage[4]).toBe('Minimus');
+        expect(Stage[5]).toBe('Doubles');
+        expect(Stage[6]).toBe('Minor');
         expect(Stage[7]).toBe('Triples');
+        expect(Stage[8]).toBe('Major');
         expect(Stage[9]).toBe('Caters');
+        expect(Stage[10]).toBe('Royal');
         expect(Stage[11]).toBe('Cinques');
+        expect(Stage[12]).toBe('Maximus');
         expect(Stage[13]).toBe('Sextuples');
+        expect(Stage[14]).toBe('Fourteen');
         expect(Stage[15]).toBe('Septuples');
+        expect(Stage[16]).toBe('Sixteen');
     });
 
 });
