b09d3c08aebcc5037e1821cc0747c0db22eabbd8
Leigh Simpson <code@simpleigh.com>
2019-01-10 20:53:41 +0000

    Pushed many exports down a level to match internal project structure

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,7 @@
  * @copyright Copyright 2015-19 Leigh Simpson. All rights reserved.
  */
 
-import {
-    AbstractBlock,
-    BlockDirectory,
-    BlockOwnership,
-    Notifiable,
-    RandomAccessContainer,
-    SerialContainer,
-} from './blocks';
+import * as Blocks from './blocks';
 import create from './create';
 import * as Music from './music';
 import Options from './Options';
@@ -24,39 +17,20 @@ import {
     Stage,
     stringFromRow,
 } from './rows';
-import {
-    Call,
-    Course,
-    methods as stedmanMethods,
-    sixes,
-    SixType,
-    Start,
-    Touch,
-} from './stedman';
+import * as Stedman from './stedman';
 import * as Visitors from './visitors';
 
 export {
-    AbstractBlock,
     Bell,
-    BlockDirectory,
-    BlockOwnership,
-    Call,
-    Course,
+    Blocks,
     create,
     Music,
-    Notifiable,
     Options,
     Pricker,
-    RandomAccessContainer,
     Row,
     rowFromString,
-    SerialContainer,
-    sixes,
-    SixType,
     Stage,
-    Start,
-    stedmanMethods,
+    Stedman,
     stringFromRow,
-    Touch,
     Visitors,
 };
