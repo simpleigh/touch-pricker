d6c1bc5987028315d4d526d39127c3c54b406907
Leigh Simpson <code@simpleigh.com>
2020-04-18 15:21:34 +0100

    Fixed test execution (`const` not recognised)

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,8 +1,12 @@
 'use strict';
 
+/* eslint-disable no-var */
+
+var testsContext;
+
 // Initial test setup
 require('./setup.ts');
 
 // Require all .spec files so they're added to the bundle
-const testsContext = require.context('../src', true, /\.spec\.ts$/);
+testsContext = require.context('../src', true, /\.spec\.ts$/);
 testsContext.keys().forEach(testsContext);
