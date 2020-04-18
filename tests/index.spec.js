81bda483a99270cc73e0a577f28e829b4c34b7a0
Leigh Simpson <code@simpleigh.com>
2020-04-18 22:45:40 +0100

    Bugfix: fixed tests broken or missing after moving matchers

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,10 +2,18 @@
 
 // Require all .spec files so they're added to the bundle
 // eslint-disable-next-line no-var
-var testsContext = require.context('../src', true, /\.spec\.ts$/);
+var contexts = [
+    require.context('../src', true, /\.spec\.ts$/),
+    require.context('./matchers', true, /\.spec\.ts$/),
+];
 
 // Initial test setup
 // eslint-disable-next-line import/no-unassigned-import
 require('./setup.ts');
 
-testsContext.keys().forEach(testsContext);
+// eslint-disable-next-line func-style
+function runContext(context) {
+    context.keys().forEach(context);
+}
+
+contexts.forEach(runContext);
--- tests/matchers/toHaveTemplate.spec.ts
+++ tests/matchers/toHaveTemplate.spec.ts
@@ -5,30 +5,23 @@
  * @copyright Copyright 2015-20 Leigh Simpson. All rights reserved.
  */
 
-import * as Templates from '..';
+import { rounds, Stage, Stedman } from '../../src';
 import toHaveTemplate from './toHaveTemplate';
 
 describe('toHaveTemplate matcher', () => {
     const compare = toHaveTemplate(jasmine.matchersUtil, []).compare;
-
-    @Templates.makePrintable({ template: () => '' })
-    class Printable implements Templates.Interface {
-
-        public print: Templates.Print;
-
-    }
-
-    const printable = new Printable();
 
     it('fails for items that are not printable', () => {
         expect(compare('string', 'template').pass).toBe(false);
     });
 
     it('fails for objects without the expected template', () => {
-        expect(compare(printable, 'other').pass).toBe(false);
+        const course = new Stedman.Course(rounds(Stage.Cinques));
+        expect(compare(course, 'other').pass).toBe(false);
     });
 
     it('passes where the template is present', () => {
-        expect(compare(printable, 'template').pass).toBe(true);
+        const course = new Stedman.Course(rounds(Stage.Cinques));
+        expect(compare(course, 'text').pass).toBe(true);
     });
 });
