53741ccbdf4bff82dcb842602f6053a749a5db5d
Leigh Simpson <code@simpleigh.com>
2023-08-11 16:08:20 +0100

    Switched from Karma to `jest` for unit test execution
    ### Why?
    
    I've stuck with [Karma](http://karma-runner.github.io/) over the years
    because I've always found it attractive to run tests in a browser.
    If all the tests run correctly in a browser then the pricker should too
    (or at least that's the theory).
    A few things have changed to make me reconsider this.
    
    * we don't tests on browsers in CI (only using PhantomJS, now JSDom)
    * PhantomJS is abandonware so we've switched to JSDom - the same as Jest
    * Karma developer experience just isn't as good - I can't filter to a
      particular test (or test file) and output is hard to understand
    * the yak-shaving to get the large volume of Karma packages on the
      latest versions was painful and time-consuming
    * some of these packages haven't been updated for a long time (e.g. I
      couldn't get the Edge runner to work at all)
    * I ran into a weird IPv4/IPv6 bug that made it hard to run tests in
      browsers locally (and I'm not sure I've satisfactorily resolved) -
      maybe Karma is becoming abandonware itself?
    * unit tests, of course, don't guarantee that the app works - we need
      an e2e testing framework to do that (and we should do that)
    
    I still like the purity of running tests in a browser with code built in
    the same way as production (i.e. by Webpack), but am leaning towards
    optimising the developer experience for unit tests (using Jest) and
    building some separate Cypress tests to actually exercise the pricker.
    
    ### What?
    
    * swapped out many packages for slightly fewer packages
    * removed some Jasmine support files
    * tweaked `package.json` scripts (mostly by removing things)
    * added `jest.config.js` and removed `karma.conf.js`
    * added `dotTransformer.js` to output doT templates for use in tests
    * removed `config/webpack.config.test.js`
    
    ### What's next?
    
    1. There's a lot of porting to be done... Many tests rely on `jasmine`
      APIs like `spyOn` or `createSpy`. They all need updating 😞
    
    2. We should also pull `dotTransformer.js` into a `dotjs-jest` NPM
      module as it isn't really part of this app.
    
    3. Finally we need to start writing those Cypress tests.
    
    ### what are we aiming for?
    
    Here's the final output from a complete test run (currently, via Karma):
    
    ```
    TOTAL: 1680 SUCCESS
    
    =============================== Coverage summary ===============================
    Statements   : 85.54% ( 2319/2711 )
    Branches     : 81.09% ( 549/677 )
    Functions    : 76.09% ( 347/456 )
    Lines        : 81.48% ( 1681/2063 )
    ================================================================================
    Done in 21.92s.
    ```

--- config/index.js
+++ config/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const paths = require('./paths');
-const webpackTestConfig = require('./webpack.config.test');
 
 module.exports = {
     paths,
-    webpackTestConfig,
 };
---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/**
+ * Free Touch Pricker
+ * @author Leigh Simpson <code@simpleigh.com>
+ * @license GPL-3.0
+ * @copyright Copyright 2015-23 Leigh Simpson. All rights reserved.
+ */
+
+'use strict';
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    transform: {
+        '^.+\\.dot$': './dotTransformer.js',
+        '^.+\\.ts$': 'ts-jest',
+    },
+};
