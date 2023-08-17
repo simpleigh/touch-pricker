fc28d125d4d35dbafa4e54123db2cf55db9b9f6e
Leigh Simpson <code@simpleigh.com>
2023-08-17 11:42:05 +0100

    Update copyright notice
    ### Why?
    
    I've recently been updating these as I edit files, but didn't bother as
    part of the introduction of Prettier (41bf325). That commit touched many
    files; let's update the copyright notice on all the remaining ones.
    
    ### What?
    
    * update copyright year to include 2023
    * drop reference to PABS as a source for `FACTORIAL` constant as this
      is a fundamental concepts within maths (and not a function)

--- tests/matchers/index.ts
+++ tests/matchers/index.ts
@@ -2,7 +2,7 @@
  * Free Touch Pricker
  * @author Leigh Simpson <code@simpleigh.com>
  * @license GPL-3.0
- * @copyright Copyright 2015-20 Leigh Simpson. All rights reserved.
+ * @copyright Copyright 2015-23 Leigh Simpson. All rights reserved.
  */
 
 import toBePrintable from './toBePrintable';
